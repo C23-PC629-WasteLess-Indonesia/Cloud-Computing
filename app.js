0fd50dabceb76c7645259004a47b4013d4fd8dbd
cahya03 <cahya03r1@gmail.com>
2023-06-15 13:07:44 +0700

    changed limit of bodyparser

--- app.js
+++ app.js
@@ -175,8 +175,8 @@ Food.belongsTo(User, { foreignKey: "userId" }); // Food memiliki relasi belongsT
 User.hasMany(Food, { foreignKey: "userId" }); // User memiliki relasi hasMany dengan Food
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: '10mb' }));
+app.use(bodyParser.json({limit: '10mb'}));
 app.use(flash());
 
 // Configure multer storage and file filter
